b7a30535ff17bd93e3482def8b15d258578ea3ca
Temmiie <mattheotermine104@gmail.com>
2025-12-15 18:02:13 +0100

    cloudflare update

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   images: {
     formats: ["image/webp"],
     qualities: [75, 80, 85, 90],
   },
   productionBrowserSourceMaps: false,
-
-  // Disable webpack cache for Cloudflare Pages (25MB file limit)
   webpack: (config) => {
-    if (process.env.CF_PAGES) {
-      config.cache = false;
-    }
     return config;
   },
 };
